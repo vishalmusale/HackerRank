80eefd282475ba52e02535d257e7747ed81cd6bb
Vishal Musale <vishal.m.musale@gmail.com>
2016-04-20 16:09:03 -0700

    Reverse a linked list

---
+++ Reverse_a_linked_list.java
@@ -0,0 +1,25 @@
+/*
+  Reverse a linked list and return pointer to the head
+  The input list will have at least one element  
+  Node is defined as  
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+    // This is a "method-only" submission. 
+    // You only need to complete this method. 
+Node Reverse(Node head) {
+    
+    if(head == null || head.next == null)
+        return head;
+    else{
+        Node curNode = head;
+        Node tempNext = curNode.next;
+        curNode.next = null;
+        
+        head = Reverse(tempNext);
+        tempNext.next = curNode;
+        return head;   
+    }
+}
